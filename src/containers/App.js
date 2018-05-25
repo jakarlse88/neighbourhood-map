02aece62c5a3b4f2fa5d6d7c5ea4f58f0668b30a
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-05-25 23:30:39 +0200

    FEAT: replace POI hardcode

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,43 +3,31 @@ import './App.css';
 import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
 
 class App extends Component {
-  render() {
+	state = {
+		points: [
+			{ name: "Moss Taekwondo Club", position: { lat: 59.420662, lng: 10.673362 }},
+			{ name: "Moss Karate Club", position: { lat: 59.431586, lng: 10.649614 }},
+			{ name: "MUDO Gym & Martial Arts Moss", position: { lat: 59.436959, lng: 10.662353 }},
+			{ name: "Dahnjun Taekwondo Klubb", position: { lat: 59.410793, lng: 10.678381 }}, 
+			{ name: "Goju Ryu Karate Rygge", position: { lat: 59.407491, lng: 10.696795 }},
+			{ name: "Moss National Taekwon-do Club", position: { lat: 59.439116, lng: 10.667983 }}
+		]
+	}
+	
+	render() {
     return (
 		<div>
 			<Map
 				google={this.props.google}
 				zoom={13}
 				initialCenter={{lat: 59.43403, lng: 10.65771}}>
-				<Marker
-					title="Moss Taekwondo Club"
-					name="Moss Taekwondo Club"
-					position={{ lat: 59.420662, lng: 10.673362 }}
-				/>
-				<Marker
-					title="Moss Karate Club"
-					name="Moss Karate Club"
-					position={{ lat: 59.431586, lng: 10.649614 }}
-				/>
-				<Marker
-					title="MUDO Gym & Martial Arts Moss"
-					name="MUDO Gym & Martial Arts Moss"
-					position={{ lat: 59.436959, lng: 10.662353 }}
-				/>
-				<Marker
-					title="Dahnjun Taekwondo Klubb"
-					name="Dahnjun Taekwondo Klubb"
-					position={{ lat: 59.410793, lng: 10.678381 }}
-				/>
-				<Marker
-					title="Goju Ryu Karate Rygge"
-					name="Goju Ryu Karate Rygge"
-					position={{ lat: 59.407491, lng: 10.696795 }}
-				/>
-				<Marker
-					title="Moss National Taekwon-do Club"
-					name="Moss National Taekwon-do Club"
-					position={{ lat: 59.439116, lng: 10.667983 }}
-				/>
+				{this.state.points.map(obj => (
+					<Marker
+						title={obj.name}
+						name={obj.name}
+						position={obj.position}
+					/>
+				))}
 			</Map>
 		</div>
     );
