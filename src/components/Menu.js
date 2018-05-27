bca958fa67872937cabc1e9bb98fddd2b33f43e0
Jon Karlsen <karlsen.jonarild@gmail.com>
2018-05-27 12:50:05 +0200

    FEAT: menu list filtering

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,7 +4,12 @@ import '../styles/Menu.css';
 export default function Menu(props) {
     return (
         <section className="menu-drawer">
-            <input id="points-filter" type="text" placeholder="Filter your search" />
+            <input 
+                id="points-filter" 
+                type="text" 
+                placeholder="Filter your search"
+                onChange = { e => props.updateQuery(e.target.value) } 
+                />
             <ul>
                 {props.points.map(obj => (
                     <li key = {obj.name}>{obj.name}</li>
--- src/containers/MenuContainer.js
+++ src/containers/MenuContainer.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import Menu from '../components/Menu';
+import escapeRegExp from 'escape-string-regexp';
 
 export default class MenuContainer extends Component {
+    state = {
+        query: ''
+    }
+
+    updateQuery = query => {
+        this.setState({ query: query });
+    }
+
     render() {
+        let showingPoints;
+
+        if (this.props.points.length > 0) {
+            const match = new RegExp(escapeRegExp(this.state.query), 'i');
+            showingPoints = this.props.points.filter(point => match.test(point.name));
+        } else {
+            showingPoints = this.props.points;
+        }
+
         return (
             this.props.showMenu && (
-                <Menu 
-                    points={this.props.points}
-                    toggleMenu={this.toggleMenu}
+                <Menu
+                    updateQuery = { this.updateQuery } 
+                    points = { showingPoints }
+                    toggleMenu = { this.toggleMenu }
                 />
             )
         )
